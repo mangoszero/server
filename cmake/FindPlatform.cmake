a46150be98c4c95fae9dde8412428f53131b3d19
Foereaper <foereaper@hotmail.com>
2015-03-29 03:31:38 +0200

    Correction to FindPlatform

--- cmake/FindPlatform.cmake
+++ cmake/FindPlatform.cmake
@@ -6,14 +6,14 @@ else()
     MESSAGE(STATUS "Detected 32-bit platform")
 endif()
 
-if(PLATFORM MATCHES X86) # 32-bit
+if(PLATFORM MATCHES 32) # 32-bit
     set(DEP_ARCH win32)
 else() # 64-bit
     set(DEP_ARCH x64)
 endif()
 
 if(XCODE)
-  if(PLATFORM MATCHES X86)
+  if(PLATFORM MATCHES 32)
     set(CMAKE_OSX_ARCHITECTURES i386)
   else()
     set(CMAKE_OSX_ARCHITECTURES x86_64)
