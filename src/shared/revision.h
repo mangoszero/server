cae572fc2a0df770ccd763c2b118ca18f82f2d91
Antz <billy1arm@users.noreply.github.com>
2015-11-10 13:55:57 +0000

    [DB] Updated Expected content level

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -38,6 +38,6 @@
 
     #define WORLD_DB_VERSION_NR 21
     #define WORLD_DB_STRUCTURE_NR 1
-    #define WORLD_DB_CONTENT_NR 6
-    #define WORLD_DB_UPDATE_DESCRIPTION "list_aura_command"
+    #define WORLD_DB_CONTENT_NR 10
+    #define WORLD_DB_UPDATE_DESCRIPTION "server-startup_fixes"
 #endif // __REVISION_H__
