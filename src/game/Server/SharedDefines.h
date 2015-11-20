066bb22948e8b589e5e0f8261323579d20b8d8b4
xfury <xfury@foxmail.com>
2015-11-20 22:48:32 +0800

    Don't interrupt certain channeled spells
    This will check for the combination of SPELL_ATTR_EX_CHANNELED_1 and SPELL_ATTR_EX3_UNK28 (possible SPELL_ATTR_EX3_ALWAYS_CAST_OK) and ignore UNIT_STAT_CAN_NOT_REACT
    
    Thanks to @vovk
    
    Close scriptdev2/scriptdev2#145
    
    (based on https://github.com/cmangos/mangos-wotlk/commit/7ced9cf483bc1e8afdc403b4a60a8ff4ff3c7912)
    
    Signed-off-by: Cala <calaftp@free.fr>
    
    (based on https://github.com/cmangos/mangos-tbc/commit/fbf73342c785a795affcd5c5d8242dd7f7720454)
    
    Signed-off-by: Cala <calaftp@free.fr>

--- src/game/Server/SharedDefines.h
+++ src/game/Server/SharedDefines.h
@@ -413,7 +413,7 @@ enum SpellAttributesEx3
     SPELL_ATTR_EX3_UNK25                        = 0x02000000,            // 25 no cause spell pushback ?
     SPELL_ATTR_EX3_UNK26                        = 0x04000000,            // 26
     SPELL_ATTR_EX3_UNK27                        = 0x08000000,            // 27
-    SPELL_ATTR_EX3_UNK28                        = 0x10000000,            // 28
+    SPELL_ATTR_EX3_UNK28                        = 0x10000000,            // 28 always cast ok ? (requires more research)
     SPELL_ATTR_EX3_UNK29                        = 0x20000000,            // 29
     SPELL_ATTR_EX3_UNK30                        = 0x40000000,            // 30
     SPELL_ATTR_EX3_UNK31                        = 0x80000000             // 31
