625ea2a6dbcf3672536495586f3a40b6ec012d4f
H0zen <H0zen@users.noreply.github.com>
2016-02-17 15:46:42 +0200

    Various external fixes - part 1     - ported fixes from cmangos repositories

--- src/game/WorldHandlers/SpellEffects.cpp
+++ src/game/WorldHandlers/SpellEffects.cpp
@@ -3022,7 +3022,7 @@ void Spell::EffectSummonPet(SpellEffectIndex eff_idx)
 
     NewSummon->SetRespawnCoord(pos);
 
-    uint32 petlevel = m_caster->getLevel();
+    uint32 petlevel = std::max(m_caster->getLevel() + m_spellInfo->EffectMultipleValue[eff_idx], 1.0f);
     NewSummon->setPetType(SUMMON_PET);
 
     uint32 faction = m_caster->getFaction();
@@ -5192,7 +5192,7 @@ void Spell::EffectBind(SpellEffectIndex eff_idx)
 
     // zone update
     data.Initialize(SMSG_PLAYERBOUND, 8 + 4);
-    data << player->GetObjectGuid();
+    data << m_caster->GetObjectGuid();
     data << uint32(area_id);
     player->SendDirectMessage(&data);
 }
