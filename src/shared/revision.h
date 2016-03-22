aa1642164e72fa2ee15a7352a47fdfd090168fb8
Antz <billy1arm@users.noreply.github.com>
2016-03-22 10:33:15 +0000

    [CharDB] match client limits.

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -32,9 +32,9 @@
     #define REALMD_DB_UPDATE_DESCRIPTION "dbdocs update"
 
     #define CHAR_DB_VERSION_NR 21
-    #define CHAR_DB_STRUCTURE_NR 3
+    #define CHAR_DB_STRUCTURE_NR 4
     #define CHAR_DB_CONTENT_NR 1
-    #define CHAR_DB_UPDATE_DESCRIPTION "Fix SoR paladin"
+    #define CHAR_DB_UPDATE_DESCRIPTION "match_client_limits"
 
     #define WORLD_DB_VERSION_NR 21
     #define WORLD_DB_STRUCTURE_NR 6
