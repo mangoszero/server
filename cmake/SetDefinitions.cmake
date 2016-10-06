1b89f1f6a1a67cadee58866258b17cb83c01ef21
H0zen <H0zen@users.noreply.github.com>
2016-10-06 20:57:46 +0300

    Revert commit [c3cbd52]. Breaks Eluna.

--- cmake/SetDefinitions.cmake
+++ cmake/SetDefinitions.cmake
@@ -41,7 +41,7 @@ if(WIN32)
     set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
     set(BIN_DIR ${CMAKE_INSTALL_PREFIX}/)
     set(LIBS_DIR ${CMAKE_INSTALL_PREFIX}/)
-    
+
     if(MSVC)
         if(PLATFORM EQUAL 64)
             # This definition is necessary to work around a bug with Intellisense described
@@ -63,11 +63,6 @@ if(WIN32)
             set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /wd4996 /wd4355 /wd4244 /wd4267 /MP")
             set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} /wd4996 /wd4355 /wd4244 /wd4985 /wd4267 /MP")
             set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /wd4996 /wd4355 /wd4244 /wd4985 /wd4267 /MP")
-        endif()
-
-        if(CMAKE_GENERATOR MATCHES "Visual Studio 14.*")
-            set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /fp:strict")
-            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /fp:strict")
         endif()
 
         # Specify the maximum PreCompiled Header memory allocation limit
@@ -94,13 +89,13 @@ if(WIN32)
 elseif(UNIX)
     set(BIN_DIR ${CMAKE_INSTALL_PREFIX}/bin)
     set(LIBS_DIR ${CMAKE_INSTALL_PREFIX}/lib)
-    
+
     # For Unix systems set the rpath so that libraries are found
     set(CMAKE_INSTALL_RPATH "${LIBS_DIR}")
     set(CMAKE_INSTALL_NAME_DIR "${LIBS_DIR}")
     # Run out of build tree
     set(CMAKE_BUILD_WITH_INSTALL_RPATH OFF)
-    
+
     # Add uninstall script and target
     configure_file(
         "${CMAKE_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
@@ -111,7 +106,7 @@ elseif(UNIX)
     add_custom_target(uninstall
         "${CMAKE_COMMAND}" -P "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake"
     )
-    
+
     if(CMAKE_C_COMPILER MATCHES "gcc" OR CMAKE_C_COMPILER_ID STREQUAL "GNU")
         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99")
 
@@ -121,7 +116,7 @@ elseif(UNIX)
             set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SSE_FLAGS}")
         endif()
         add_definitions(-DHAVE_SSE2)
-        
+
         if(NOT DEBUG)
             set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} --no-warnings")
             set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} --no-warnings -std=c++11 -Wno-narrowing -Wno-deprecated-register")
