7f39938f6ca6c0ee6575d5833640ea032428ffdf
Antz <billy1arm@users.noreply.github.com>
2016-11-24 08:16:33 +0000

    [Build] Added MySQL 5.7 support

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -36,6 +36,7 @@ else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
     "${PROGRAM_FILES_ARCH_PATH}/MySQL/MySQL Server 5.4/include"
     "${PROGRAM_FILES_ARCH_PATH}/MySQL/MySQL Server 5.5/include"
     "${PROGRAM_FILES_ARCH_PATH}/MySQL/MySQL Server 5.6/include"
+    "${PROGRAM_FILES_ARCH_PATH}/MySQL/MySQL Server 5.7/include"
   )
 
   if(WIN32 AND MSVC)
@@ -50,6 +51,7 @@ else(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
         "${PROGRAM_FILES_ARCH_PATH}/MySQL/MySQL Server 5.4/lib"
         "${PROGRAM_FILES_ARCH_PATH}/MySQL/MySQL Server 5.5/lib"
         "${PROGRAM_FILES_ARCH_PATH}/MySQL/MySQL Server 5.6/lib"
+        "${PROGRAM_FILES_ARCH_PATH}/MySQL/MySQL Server 5.7/lib"
       )
   else(WIN32 AND MSVC)
     find_library(MYSQL_LIBRARY
