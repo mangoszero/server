45432f4f8c9a5cfdf49e88c037c8d9dfbffb2ad0
Blumfield <kaiosown@gmail.com>
2017-01-16 16:52:45 +0100

    [Spell] Remove vanish buff is user cancel stealth

--- src/game/WorldHandlers/SpellAuras.cpp
+++ src/game/WorldHandlers/SpellAuras.cpp
@@ -2533,6 +2533,10 @@ void Aura::HandleModStealth(bool apply, bool Real)
         if (Real && target->GetTypeId() == TYPEID_PLAYER && GetId() == 20580)
             { target->RemoveAurasDueToSpell(21009); }
 
+        // Remove vanish buff if user cancel stealth
+        if (m_removeMode == AURA_REMOVE_BY_CANCEL)
+            target->RemoveSpellsCausingAura(SPELL_AURA_MOD_STEALTH);
+
         // only at real aura remove of _last_ SPELL_AURA_MOD_STEALTH
         if (Real && !target->HasAuraType(SPELL_AURA_MOD_STEALTH))
         {
