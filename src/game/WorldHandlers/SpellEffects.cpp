c3e483abb6427781bc10df8df233386330054084
mpfans <mpfans@vip.qq.com>
2017-01-23 07:32:00 +0000

    Fixed Tamed Kodo 5 automatically disappear

--- src/game/WorldHandlers/SpellEffects.cpp
+++ src/game/WorldHandlers/SpellEffects.cpp
@@ -746,6 +746,17 @@ void Spell::EffectDummy(SpellEffectIndex eff_idx)
                     m_caster->CastSpell(unitTarget, spell_list[urand(0, 5)], true);
                     return;
                 }
+                case 18269:                                 // Kodo Kombobulator
+                {
+                    if (!unitTarget)
+                        { return; }
+
+                    if (unitTarget->GetTypeId() == TYPEID_PLAYER)
+                        { return; }
+
+                        ((Creature*)unitTarget)->ForcedDespawn();
+                        return;
+                }
                 case 18350:                                 // Dummy Trigger
                 {
                     if (unitTarget->GetTypeId() != TYPEID_PLAYER)
