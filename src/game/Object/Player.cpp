8c43f2a66845cfd219a20e72b75584f6d9df016a
Lee <littlelee_1991@live.co.uk>
2017-02-28 19:09:09 +0000

    Update Player
    when gm is activated players can not attack pets.

--- src/game/Object/Player.cpp
+++ src/game/Object/Player.cpp
@@ -2204,12 +2204,27 @@ void Player::SetGameMaster(bool on)
 
         GetHostileRefManager().setOnlineOfflineState(false);
         CombatStopWithPets();
+        
+        if (Pet* pet = GetPet())
+        {
+            if (m_ExtraFlags |= PLAYER_EXTRA_GM_ON)
+                pet->setFaction(35);
+            pet->GetHostileRefManager().setOnlineOfflineState(false);
+        }
+	   
+
     }
     else
     {
         m_ExtraFlags &= ~ PLAYER_EXTRA_GM_ON;
         setFactionForRace(getRace());
         RemoveFlag(PLAYER_FLAGS, PLAYER_FLAGS_GM);
+        
+        if (Pet* pet = GetPet())
+        {
+            pet->setFaction(getFaction());
+            pet->GetHostileRefManager().setOnlineOfflineState(true);
+        }
 
         CallForAllControlledUnits(SetGameMasterOffHelper(getFaction()), CONTROLLED_PET | CONTROLLED_TOTEMS | CONTROLLED_GUARDIANS | CONTROLLED_CHARM);
 
