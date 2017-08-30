84476439370265dcbfe4b0f278af2ee2c3c45e42
Wolverine <wolverine79936@hotmail.com>
2017-08-30 13:39:16 -0600

    Continuation of commit 1f1735ad0c81a6740a6345c0b7ae1c5d195628c6
    Just some minor cleanup stuff. This commit should finally be complete.

--- src/game/AuctionHouseBot/AuctionHouseBot.cpp
+++ src/game/AuctionHouseBot/AuctionHouseBot.cpp
@@ -841,7 +841,6 @@ bool AuctionBotBuyer::IsBuyableEntry(uint32 buyoutPrice, double InGame_BuyPrice,
 
 bool AuctionBotBuyer::IsBidableEntry(uint32 bidPrice, double InGame_BuyPrice, double MaxBidablePrice, uint32 MinBidPrice, uint32 MaxChance, uint32 ChanceRatio)
 {
-    double ratio = 0;
     uint32 Chance = 0;
 
     if (bidPrice <= MinBidPrice)
@@ -852,7 +851,7 @@ bool AuctionBotBuyer::IsBidableEntry(uint32 bidPrice, double InGame_BuyPrice, do
         {
             if (bidPrice < MaxBidablePrice)
             {
-                ratio = MaxBidablePrice / bidPrice;
+                double ratio = MaxBidablePrice / bidPrice;
                 if (ratio < 3)
                     { Chance = ((MaxChance / 500) * ratio); }
                 else
@@ -866,7 +865,7 @@ bool AuctionBotBuyer::IsBidableEntry(uint32 bidPrice, double InGame_BuyPrice, do
     {
         if (bidPrice < MaxBidablePrice)
         {
-            ratio = MaxBidablePrice / bidPrice;
+            double ratio = MaxBidablePrice / bidPrice;
             if (ratio < 4)
                 { Chance = ((MaxChance / 1000) * ratio); }
             else
