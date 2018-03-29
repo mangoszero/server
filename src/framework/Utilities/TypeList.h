e16efe3fbbf9d30a959c178291af7090b935eba9
H0zen <compteh@ub.ro>
2018-03-29 17:05:05 +0300

    Mark TypeList.h for deletion

--- src/framework/Utilities/TypeList.h
+++ src/framework/Utilities/TypeList.h
@@ -25,34 +25,6 @@
 #ifndef MANGOS_TYPELIST_H
 #define MANGOS_TYPELIST_H
 
-class TypeNull;
-
-template<typename HEAD, typename TAIL>
-/**
- * @brief TypeList is the most simple but yet the most powerfull class of all.
- *
- * It holds at compile time the different type of objects in a linked list.
- *
- */
-struct TypeList
-{
-    /**
-     * @brief
-     *
-     */
-    typedef HEAD Head;
-    /**
-     * @brief
-     *
-     */
-    typedef TAIL Tail;
-};
-
-// enough for now.. can be expand at any point in time as needed
-#define TYPELIST_1(T1)                 TypeList<T1, TypeNull>
-#define TYPELIST_2(T1, T2)             TypeList<T1, TYPELIST_1(T2) >
-#define TYPELIST_3(T1, T2, T3)         TypeList<T1, TYPELIST_2(T2, T3) >
-#define TYPELIST_4(T1, T2, T3, T4)     TypeList<T1, TYPELIST_3(T2, T3, T4) >
-#define TYPELIST_5(T1, T2, T3, T4, T5) TypeList<T1, TYPELIST_4(T2, T3, T4, T5) >
+// marked for deletion
 
 #endif
