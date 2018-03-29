014c645cf3b079e8dca5435e80971fe2114b1cbb
H0zen <compteh@ub.ro>
2018-03-29 15:37:44 +0300

    Introducing std::unordered_map/set and enforce C++11 on Linux

--- cmake/SetDefinitions.cmake
+++ cmake/SetDefinitions.cmake
@@ -109,7 +109,7 @@ elseif(UNIX)
     )
 
     if(CMAKE_C_COMPILER MATCHES "gcc" OR CMAKE_C_COMPILER_ID STREQUAL "GNU")
-        set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 
         if(PLATFORM EQUAL 32)
             set(SSE_FLAGS "-msse2 -mfpmath=sse")
