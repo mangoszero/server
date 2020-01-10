28c979b607759e8df7105839875ddaf02a931131
Antz <billy1arm@users.noreply.github.com>
2020-01-10 09:42:12 +0000

    test3

--- .travis.sh
+++ .travis.sh
@@ -5,6 +5,6 @@ set -eu
 if [[ x$OSTYPE =~ ^xdarwin ]]; then
   brew update
   brew install "mysql"
-  brew install "openssl"
+  brew reinstall openssl@1.1
   cmake -DOPENSSL_ROOT_DIR=/usr/local/ssl -DOPENSSL_LIBRARIES=/usr/local/ssl/lib
 fi
