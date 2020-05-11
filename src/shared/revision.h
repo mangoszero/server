c8950d11048c5d14071593e59fd03b94479d6eb4
Elmsroth <pieric62@gmail.com>
2020-05-11 09:35:20 +0100

    Adjust Revision number

--- src/shared/revision.h
+++ src/shared/revision.h
@@ -38,6 +38,6 @@
 
     #define WORLD_DB_VERSION_NR 21
     #define WORLD_DB_STRUCTURE_NR 17
-    #define WORLD_DB_CONTENT_NR 053
+    #define WORLD_DB_CONTENT_NR 001
     #define WORLD_DB_UPDATE_DESCRIPTION "GM_tickets_handling_fixes_pt1"
 #endif // __REVISION_H__
