8334596382aa454e951f232bdf999f55bf6255c0
Antz <billy1arm@users.noreply.github.com>
2020-11-25 19:50:04 +0000

    PLAYER_EVENT_ON_LOOT_ITEM fix for eluna. Thanks mostlikey

--- src/game/WorldHandlers/LootHandler.cpp
+++ src/game/WorldHandlers/LootHandler.cpp
@@ -216,6 +216,10 @@ void WorldSession::HandleAutostoreLootItemOpcode(WorldPacket& recv_data)
         --loot->unlootedCount;
 
         player->SendNewItem(newitem, uint32(item->count), false, false, true);
+
+#ifdef ENABLE_ELUNA
+        sEluna->OnLootItem(player, newitem, item->count, lguid);
+#endif /* ENABLE_ELUNA */
     }
     else
     {
