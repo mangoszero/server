12e593f345c43402cc5c1049d0613bf543be3160
Antz <billy1arm@users.noreply.github.com>
2021-03-05 08:37:12 +0000

    Add the rest of the components to build to build script

--- apps/ci/ci-compile.sh
+++ apps/ci/ci-compile.sh
@@ -10,7 +10,7 @@ test -d _install || mkdir _install
 cd _build
 
 # Run CMake Configurations
-cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DSOAP=1 -DPLAYERBOTS=1 -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/
+cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_TOOLS:BOOL=1 -DBUILD_MANGOSD:BOOL=1 -DBUILD_REALMD:BOOL=1 -DSOAP:BOOL=1 -DSCRIPT_LIB_ELUNA:BOOL=1 -DSCRIPT_LIB_SD3:BOOL=1 -DPLAYERBOTS:BOOL=1 -DUSE_STORMLIB:BOOL=1 -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl/
 
 # Check OpenSSL
 which openssl
