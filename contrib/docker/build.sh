53b7fee3182519152546fd63f63bd6c8a9d85dc5
AndyBe <andreas.benzler@gmail.com>
2021-05-09 11:10:50 +0200

    removed mangos user from build script

--- contrib/docker/build.sh
+++ contrib/docker/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 CMAKE_COMMANDS='-DCMAKE_INSTALL_PREFIX=/app -DCONF_INSTALL_DIR=/app/etc -DBUILD_EXTRACTORS=ON -DBUILD_PLAYERBOT=ON'
 
-# we need to create same user as on realm and world
- useradd -m -d /home/mangos  -c "MaNGOS" -U mangos
-
 # container application 
 if [ ! -d "app/bin" ]; then
   mkdir -p app/bin
@@ -11,25 +8,17 @@ fi
 
 # needed for mangos container - from docker-compose build
 if [ ! -f "app/bin/mangosd-entrypoint.sh" ]; then
-
    cp mangos/contrib/docker/world/mangosd-entrypoint.sh app/bin/mangosd-entrypoint.sh
-   chown mangos:mangos mangosd-entrypoint.sh 
-
 fi
 
 # docker env
 if [ ! -f "mangos.env" ]; then
-
    cp mangos/contrib/docker/mangos.env .
-   chown mangos:mangos mangos.env
-
 fi
 
 # the compose file for all 3 containers (db /realm /world)
 if [ ! -f "docker-compose.yml" ]; then
    cp mangos/contrib/docker/docker-compose.yml .
-   chown mangos:mangos docker-compose.yml 
-
 fi
 
 # update git source
@@ -53,5 +42,3 @@ make -j$(nproc --all)
 # /app/bin/{tools}
 # /app/etc
 make install
-# make 
-chown -R mangos:mangos /app
