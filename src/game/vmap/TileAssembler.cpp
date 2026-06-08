609a02d7771c906690198ebc2404a2ec814737cb
billy1arm <antz@cix.co.uk>
2026-06-08 20:49:58 +0100

    Maps/Vmaps and MMaps Digit change

--- src/game/vmap/TileAssembler.cpp
+++ src/game/vmap/TileAssembler.cpp
@@ -174,7 +174,7 @@ namespace VMAP
 
             // Write map tree file
             std::stringstream mapfilename;
-            mapfilename << iDestDir << "/" << std::setfill('0') << std::setw(3) << map_iter->first << ".vmtree";
+            mapfilename << iDestDir << "/" << std::setfill('0') << std::setw(4) << map_iter->first << ".vmtree";
             FILE* mapfile = fopen(mapfilename.str().c_str(), "wb");
             if (!mapfile)
             {
@@ -232,7 +232,7 @@ namespace VMAP
                 uint32 nSpawns = tileEntries.count(tile->first);
                 std::stringstream tilefilename;
                 tilefilename.fill('0');
-                tilefilename << iDestDir << "/" << std::setw(3) << map_iter->first << "_";
+                tilefilename << iDestDir << "/" << std::setw(4) << map_iter->first << "_";
                 uint32 x, y;
                 StaticMapTree::unpackTileID(tile->first, x, y);
                 tilefilename << std::setw(2) << x << "_" << std::setw(2) << y << ".vmtile";
