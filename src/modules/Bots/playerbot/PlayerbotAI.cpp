eae9bb4b3961cf13ba12338121e6ec91997bfc00
bo <bo@zimmers.net>
2026-06-30 19:35:55 -0500

    invite fix

--- src/modules/Bots/playerbot/PlayerbotAI.cpp
+++ src/modules/Bots/playerbot/PlayerbotAI.cpp
@@ -129,10 +129,10 @@ PlayerbotAI::PlayerbotAI(Player* bot)
     masterIncomingPacketHandlers.AddHandler(CMSG_MOVE_SPLINE_DONE, "taxi done");
     masterIncomingPacketHandlers.AddHandler(CMSG_GROUP_UNINVITE_GUID, "uninvite");
     masterIncomingPacketHandlers.AddHandler(CMSG_PUSHQUESTTOPARTY, "quest share");
-    masterIncomingPacketHandlers.AddHandler(CMSG_GUILD_INVITE, "guild invite");
     masterIncomingPacketHandlers.AddHandler(CMSG_REPOP_REQUEST, "master released spirit");
     masterIncomingPacketHandlers.AddHandler(CMSG_SPIRIT_HEALER_ACTIVATE, "master spirit healer");
 
+    botOutgoingPacketHandlers.AddHandler(SMSG_GUILD_INVITE, "guild invite");
     botOutgoingPacketHandlers.AddHandler(SMSG_GROUP_INVITE, "group invite");
     botOutgoingPacketHandlers.AddHandler(BUY_ERR_NOT_ENOUGHT_MONEY, "not enough money");
     botOutgoingPacketHandlers.AddHandler(BUY_ERR_REPUTATION_REQUIRE, "not enough reputation");
